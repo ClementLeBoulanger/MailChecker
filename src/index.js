93d901a89ba034cd9dc0d69a24443d1794693c62
Clement Le Boulanger <hello@clement-leboulanger.com>
2022-03-16 14:58:56 +0100

    A refacto

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import { SubjectInput, BodyInput } from './variables';
 import { SubjectTest } from './subject-test';
 import { BodyTest } from './body-test';
 
-const btnSend = document.querySelector('button')
-
-
+const btnSend = document.querySelector('#btn-send')
+const divGlobalScore = document.querySelector('#globalscore')
 
 btnSend.addEventListener('click', event => {
   event.preventDefault()
@@ -14,8 +13,32 @@ btnSend.addEventListener('click', event => {
   let subjectScore = SubjectTest(subjectInput)
   let bodyScore = BodyTest(bodyInput)
 
-  console.log(subjectScore)
-  console.log(bodyScore)
-  let globalScore = ((subjectScore + bodyScore)/12) * 10
-  console.log(`Your mail's score is ${Math.round(globalScore)} / 10`)
+  let globalScore = Math.round(((subjectScore + bodyScore)/12) * 10)
+
+  btnSend.style.display = "none";
+
+  divGlobalScore.insertAdjacentHTML('beforeend', '<div id="gauge" class="gauge"></div>')
+
+  const g = new JustGage({
+    id: "gauge",
+    value: globalScore,
+    min: 0,
+    max: 10,
+    valueFontColor: "rgb(255, 0, 89)",
+    labelFontColor: "rgb(23, 231, 217)",
+    counter: true,
+    customSectors: {
+      percents: true, // lo and hi values are in %
+      ranges: [{
+        color: "#FF3B30",
+        lo: 0,
+        hi: 50
+      },
+      {
+        color: "#3BFF30",
+        lo: 51,
+        hi: 100
+      }]
+    }
+  });
 });
--- src/test/second-line-question.js
+++ src/test/second-line-question.js
@@ -1,6 +1,7 @@
 const SecondLineQuestion = (input) => {
   const model = /^.*\n{1}.*[?]+/
   let result = model.test(input)
+
   return result
 }
 
